fe156a80c7927d66d891b199cd5e68497b5a47a2
Xyzic <het1998@hotmail.com>
2021-11-30 15:14:09 -0600

    Tester TO-DO

---
+++ jupyterlab_nodeeditor/yggdrasil_tester.py
@@ -0,0 +1,20 @@
+# TO-DO
+# Transfer Tests from yggdrasil_support.py
+# Create more tests to ensure the model was properly put in
+# # Test to make sure all important values were extracted
+# # # Inputs
+# # # Outputs
+# # # Title
+# # Test all JLNE components added
+# # # Slots
+# # # Editor
+# Make sure Vis is correct (Manual for now as I learn the JLNE output coding)
+
+PASS = "Passed"
+FAIL = "Failed"
+
+# I will start with just Photosynthesis model testing, then move on to develop and test custom models
+
+class YggNodeTester:
+
+    def __init__(self):

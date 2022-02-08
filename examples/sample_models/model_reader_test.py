8aa5e6930132b48e16fb0ca8cccf116adbe6fd19
Xyzic <het1998@hotmail.com>
2022-02-08 16:58:33 -0600

    Finished Reader

--- examples/sample_models/model_reader_test.py
+++ examples/sample_models/model_reader_test.py
@@ -1,12 +1,23 @@
-import urllib
-import time
+from yggdrasil.interface.YggInterface import YggInput, YggOutput
 
-def model_book():
-    # Change this to "emit"
-    file = urllib.request.urlopen("https://www.gutenberg.org/files/74/74-0.txt")
-    i = 0
-    for line in file:
-        decoded_line = line.decode("utf-8")
-        i += 1
-        print(i, decoded_line)
-        time.sleep(2)
+# If we want to do remote reading, we can try reading in the url as its own model
+# Then transfer that model to another model to read it in ygg
+# This is done locally
+
+if __name__ == '__main__':
+    in_file = YggInput('inputReader_file')
+    out_file = YggOutput('outputReader_file')
+    line_number = 0
+    ret = True
+
+    # Output the text line by line with the line number
+    while ret:
+        (ret, line) = in_file.recv()
+        if ret:
+            line_number += 1
+            ret = out_file.send(line_number, line)
+            if not ret:
+                raise RuntimeError("ERROR SENDING LINE")
+        else:
+            print("End of File")
+            out_file.send_eof()

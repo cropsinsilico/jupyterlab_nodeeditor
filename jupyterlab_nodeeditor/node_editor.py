5481421db52e05c568847761429e3944cf048de6
Matthew Turk <matthewturk@gmail.com>
2022-12-20 09:08:24 -0600

    Starting syncing of values back and forth

--- jupyterlab_nodeeditor/node_editor.py
+++ jupyterlab_nodeeditor/node_editor.py
@@ -120,13 +120,13 @@ class DropDownInputControlModel(InputControlModel):
 @ipywidgets.register
 class NumberInputControlModel(InputControlModel):
     _model_name = traitlets.Unicode("ReteNumControlModel").tag(sync=True)
-    initial_value = traitlets.CInt().tag(sync=True)
+    value = traitlets.CInt().tag(sync=True)
 
 
 @ipywidgets.register
 class TextInputControlModel(InputControlModel):
     _model_name = traitlets.Unicode("ReteTextControlModel").tag(sync=True)
-    initial_value = traitlets.Unicode().tag(sync=True)
+    value = traitlets.Unicode().tag(sync=True)
 
 
 @ipywidgets.register

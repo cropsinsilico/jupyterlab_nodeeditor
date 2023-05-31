26b1d1e2aa50868caedcc021199d51b63c3d7abc
YilinXia <xia.yl@outlook.com>
2023-05-31 14:55:14 -0500

    align node-controls

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+
+# This is a script to automate the installation process
+
+# run jlpm install
 jlpm install
+
+# run jupyter labextension develop
 jupyter labextension develop . --overwrite
+
+# run jlpm build
 jlpm run build
+
+# run pip install
 pip install -e .
+
+# install yggdrasil-framework
 pip install yggdrasil-framework

31c804bb2de7a2a2919afd5b2ef3820044a41a1d
Yilin Xia <xia.yl@outlook.com>
2023-09-17 22:14:56 +0000

    simplify devcontainer setup

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
 
-sudo apt update
+pip install --upgrade pip
 
-#install node and npm
-sudo apt install nodejs npm --yes
-
-#update node to the latest
-nvm install node
-nvm use node
-nvm alias default node
-
-# update several packages exist in the original image
-pip install --upgrade spyder spyder-kernels conda-repo-cli
-
+pip install jupyterlab
